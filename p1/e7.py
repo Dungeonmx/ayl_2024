1d0c5418896382e6aeb99cec8abf1718a9a4e9db
Dungeonmx <guzmandiegobenjamin@gmail.com>
2024-09-01 11:24:19 -0300

    e7 finalizado

--- p1/e7.py
+++ p1/e7.py
@@ -7,7 +7,36 @@ def convert(input_file_path: str, output_file_path: str) -> None:
     :param output_file_path: csv output file
     :return:
     """
-    pass
+    group = r"^(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*)$"
+
+    group = re.compile(group)
+
+    email = r"@[!#$%&'*+-/=?^_`{|}~\w]*\.\w*(\.\w*)?"
+
+    email = re.compile(email)
+
+    with open(input_file_path, "r") as inputf, open(output_file_path, "w") as outputf:
+        new_text = []
+        text = inputf.readlines()
+        #text = re.sub(email, "@ing.unlpam.edu.ar", text)
+        #print(text)
+        for line in text:
+            format_line = group.match(line)
+            new_text.append("{},{},{},{},{},{},{},{}\n".format(
+                format_line.group(1),
+                format_line.group(3),
+                format_line.group(2),
+                email.sub("@ing.unlpam.edu.ar",format_line.group(4)),
+                format_line.group(6),
+                format_line.group(7),
+                format_line.group(8),
+                format_line.group(9)
+            ))
+        print(new_text)
+        outputf.writelines(new_text)
+
+
+
 
 
 if __name__ == '__main__':
